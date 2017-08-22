65710d594e31e346c51fbdc93783c3c4a1a49a36
Hector Ochoa <hecdan8@hotmail.com>
2017-08-22 17:18:30 -0500

    fix security group port range

--- EA/sg.tf
+++ EA/sg.tf
@@ -5,21 +5,21 @@ resource "aws_security_group" "instance_sg" {
 
   ingress {
     protocol    = "tcp"
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     protocol    = "tcp"
-    from_port   = 0
+    from_port   = 443
     to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     protocol    = "tcp"
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
